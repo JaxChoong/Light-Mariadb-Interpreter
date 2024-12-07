fc8e0fae3ecc03ae7b25f92f0a8b5b801d0aee48
iconicstixx <ccalvinoutlookcom@gmail.com>
2024-12-07 18:30:06 +0800

    chore: updated crudentials

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
-// *******************************************************
-// Program : main.cpp
-// Course : CCP6114 Programming Fundamentals
-// Lecture Class : TC3L
-// Tutoral Class : TT5L
+// *****************************************************************************************
+// Course: CCP6114 Programming Fundamentals
+// Lecture Class: TC3L
+// Tutorial Class : TT5L
 // Trimester : 2430
-// Member_1 : 242UC244JT | CHOONG JIA XUEN | 013-657 3888
-// Member_2 : 242UC244 | CHONG MENG HANG | 016-871 1296
-// Member_3 : 242UC244 | KEE HUI YEE | 016-624 0924
-// Member_4 : 242UC244 | CHONG MENG HANG | 016-871 1296
+// Member_1 : 242UC244K1 || Choong Jia Xuen || choong.jia.xuen@student.mmu.edu.my || 013-657 3888
+// Member_2 : 242UC244GS || Kee Hui Yee || keehuiyee@student.mmu.edu.my || 016-624 0924
+// Member_3 : 242UC244JT || Chong Meng Hang || chong.meng.hang@student.mmu.edu.my || 016-871 1296 
+// Member_4 : ID || Hean Qi Shen || email || Phone
+
+// Task Distribution
+// Member_1 :
+// Member_2 :
+// Member_3 :
+// Member_4 :
+
+// *****************************************************************************************
