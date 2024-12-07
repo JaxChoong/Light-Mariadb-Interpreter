4a7606eda58dc27fef98717c5ae4c87fc6a04b99
iconicstixx <ccalvinoutlookcom@gmail.com>
2024-12-07 17:55:16 +0800

    chore: added crudentials on the first cpp file

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// *******************************************************
+// Program : main.cpp
+// Course : CCP6114 Programming Fundamentals
+// Lecture Class : TC3L
+// Tutoral Class : TT5L
+// Trimester : 2430
+// Member_1 : 242UC244JT | CHOONG JIA XUEN | 013-657 3888
+// Member_2 : 242UC244 | CHONG MENG HANG | 016-871 1296
+// Member_3 : 242UC244 | KEE HUI YEE | 016-624 0924
+// Member_4 : 242UC244 | CHONG MENG HANG | 016-871 1296
