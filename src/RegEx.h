1062af4c6c9576ae3d84c298a62cf5813e79e42f
JaxChoong <choongjax@gmail.com>
2024-12-24 16:45:15 +0800

    feat: now can see what type of create

--- src/RegEx.h
+++ src/RegEx.h
@@ -8,6 +8,30 @@
 
 using namespace std;
 
+vector<string> get_create_type(const string& create_command) {
+    regex database_command("(DATABASE)(.*)");
+    regex table_command("(TABLE)(.*)");
+    regex output_file_command("(.*).txt");
+
+    smatch m;
+    if (regex_search(create_command, m, database_command)) {
+        cout << "Create database" << endl;
+        string database_name = "";
+        return {"database_name" , database_name};
+    }
+    else if (regex_search(create_command, m, table_command)) {
+        cout << "Create table" << endl;
+        string table_name = "";
+        return {"table_name" , table_name};
+    }
+    else if (regex_search(create_command, m, output_file_command)) {
+        string output_file_name = m[1].str() + ".txt";
+        cout << "Output file name: " << output_file_name << endl;
+        return {"output_file_name", output_file_name};
+    }
+    return {};
+}
+
 void process_line(const string& line) {
     regex create_command("(CREATE)(.*)");
     regex drop_command("(DROP)(.*)");
@@ -20,7 +44,12 @@ void process_line(const string& line) {
 
     smatch m;
     if (regex_search(line, m, create_command)) {
-        cout << "Create this" << endl;
+        vector<string> create_type_vector = get_create_type(m[2]);
+        string create_type = create_type_vector[0];
+        string create_name = create_type_vector[1];
+        cout << create_type << create_name << endl;
+
+
     }
     if (regex_search(line, m, drop_command)) {
         cout << "Drop this" << endl;
