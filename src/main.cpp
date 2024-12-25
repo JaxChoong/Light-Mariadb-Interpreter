bdb423ed6592a8030bde76bb84d52435a188411e
JaxChoong <choongjax@gmail.com>
2024-12-25 15:11:45 +0800

    chore: renamed the table in fileInput1.mdb, and added task delegation in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,12 @@
 // Member_1 : 242UC244K1 || Choong Jia Xuen || choong.jia.xuen@student.mmu.edu.my || 013-657 3888
 // Member_2 : 242UC244GS || Kee Hui Yee || keehuiyee@student.mmu.edu.my || 016-624 0924
 // Member_3 : 242UC244JT || Chong Meng Hang || chong.meng.hang@student.mmu.edu.my || 016-871 1296 
-// Member_4 : ID || Hean Qi Shen || email || Phone
+// Member_4 : 242UC244FW || Hean Qi Shen || jason.hean.qi@student.mmu.edu.my || 019-214 1180
 
 // Task Distribution
-// Member_1 :
-// Member_2 :
-// Member_3 :
+// Member_1 : FileRead.h, main.cpp, RegEx.h
+// Member_2 : main.cpp, RegEx.h
+// Member_3 : RegEx.h
 // Member_4 :
 
 // *****************************************************************************************
