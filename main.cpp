8167af90e1144f26a2ea668c60b3653ade19a2b7
JaxChoong <choongjax@gmail.com>
2025-01-13 09:15:28 +0800

    feat: cleaned up main.cpp

--- main.cpp
+++ main.cpp
@@ -26,54 +26,8 @@
 
 using namespace std;
 
-// Function prototypes
-vector<string> get_database_files(); // Gets all the database files in the Data folder
-string choose_database(vector<string> database_files); // Allows user to choose a database file
-void read_file(const string& filename); // Reads the specified file
-
 int main() {
     string current_database = "fileInput1.mdb"; // Currently you can only fixate the database file here
     read_file(current_database); // Read file (main program loop)
     return 0;
-}
-
-// Gets all the database files in the Data folder
-vector<string> get_database_files() {
-    vector<string> database_files;
-    filesystem::current_path(filesystem::path(__FILE__).parent_path());
-
-    for (const auto& entry : filesystem::directory_iterator("../Data")) 
-    {
-        database_files.push_back(entry.path().filename().string());
-    }
-
-    cout << "Databases found:" << endl;
-    int i = 0;
-
-    for (const auto& file : database_files) 
-    {
-        cout << "- "<< file << " ( Select with " << to_string(i) << " )"<< endl;
-        i++;
-    }
-
-    return database_files;
-}
-
-// Allows user to choose a database file
-string choose_database(vector<string> database_files) {
-    string database;
-    while (true) 
-    {
-        cout << "Choose a database: ";
-        cin >> database;
-        if (stoi(database) < database_files.size()) 
-        {
-            cout << "Selected database: " << database_files[stoi(database)] << endl <<endl;
-            return database_files[stoi(database)];
-        } 
-        else 
-        {
-            cout << "Invalid choice. Please try again." << endl;
-        }
-    }
 }
