ba043090e29b17c28df2691796311090ecdc0cfd
JaxChoong <choongjax@gmail.com>
2025-01-16 08:51:39 +0800

    style: removed whitespace before ' for fileInput3

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 // Trimester : 2430
 // Member_1 : 242UC244K1 || Choong Jia Xuen || choong.jia.xuen@student.mmu.edu.my || 013-657 3888
 // Member_2 : 242UC244GS || Kee Hui Yee || keehuiyee@student.mmu.edu.my || 016-624 0924
-// Member_3 : 242UC244JT || Chong Meng Hang || chong.meng.hang@student.mmu.edu.my || 016-871 1296 
+// Member_3 : 242UC244JT || Chong Meng Hang || chong.meng.hang@student.mmu.edu.my || 016-871 1296
 // Member_4 : 242UC244FW || Hean Qi Shen || jason.hean.qi@student.mmu.edu.my || 019-214 1180
 
 // Task Distribution
