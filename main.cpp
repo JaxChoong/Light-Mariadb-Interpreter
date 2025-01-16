b519d596a5e45b091452ea8d1691d4c407ef9d2c
Jason <jacksonlightning95@gmail.com>
2025-01-16 08:32:29 +0800

    testing

--- main.cpp
+++ main.cpp
@@ -6,13 +6,13 @@
 // Member_1 : 242UC244K1 || Choong Jia Xuen || choong.jia.xuen@student.mmu.edu.my || 013-657 3888
 // Member_2 : 242UC244GS || Kee Hui Yee || keehuiyee@student.mmu.edu.my || 016-624 0924
 // Member_3 : 242UC244JT || Chong Meng Hang || chong.meng.hang@student.mmu.edu.my || 016-871 1296 
-// Member_4 : 242UC244FW || Hean Qi Shen || jason.hean.qi@student.mmu.edu.my || 019-214 1180
+// Member_4 : 242UC244FW || Jason Hean Qi Shen || jason.hean.qi@student.mmu.edu.my || 019-214 1180
 
 // Task Distribution
 // Member_1 : FileRead.h, main.cpp, RegEx.h
 // Member_2 : main.cpp, RegEx.h
 // Member_3 : RegEx.h
-// Member_4 :
+// Member_4 : RegEx.h
 
 // *****************************************************************************************
 
@@ -23,7 +23,7 @@
 using namespace std;
 
 int main() {
-    string current_database = "fileInput1.mdb"; // Currently you can only fixate the database file here
+    string current_database = "fileInput2.mdb"; // Currently you can only fixate the database file here
     read_file(current_database); // Read file (main program loop)
     return 0;
 }
